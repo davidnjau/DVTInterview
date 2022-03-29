b393532880956b165c93cf3e7738d763b003633d
Dave <davidnjau21@gmail.com>
2022-03-29 10:16:35 +0300

    Splash image

--- app/src/main/java/com/example/dvt/activities/Splash.kt
+++ app/src/main/java/com/example/dvt/activities/Splash.kt
@@ -4,21 +4,31 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dvt.R
-import com.example.dvt.roomdatabase.RoomViewModel
+import java.util.*
 
 
 @Suppress("DEPRECATION")
 class Splash : AppCompatActivity() {
 
+    private lateinit var mainBackground: RelativeLayout
+    var myarray = intArrayOf(R.drawable.morning, R.drawable.midday, R.drawable.night)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        mainBackground = findViewById(R.id.mainBackground)
+
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         //This method is used so that your splash activity
         //can cover the entire screen.
+
+        val r = Random()
+        val i: Int = r.nextInt(2)
+        this@Splash.window.setBackgroundDrawableResource(myarray[i])
 
         Handler().postDelayed({
             val i = Intent(this@Splash, MainActivity::class.java)
